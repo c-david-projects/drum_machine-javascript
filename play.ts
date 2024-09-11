33c2064d61da90e73763c0110c85eee294f80341
César Lima <cdavidlima@gmail.com>
2024-09-11 23:34:33 +0100

    Update play.ts
    Remove deprecated feature

--- play.ts
+++ play.ts
@@ -35,31 +35,6 @@ document.addEventListener("DOMContentLoaded", function () {
             }, 500);
         }
     });
-
-    let btn = document.getElementById("btn");
-    const text = document.getElementById("text");
-
-    function getRandomSentence() {
-        let index = Math.floor(Math.random() * sentences.length);
-        return sentences[index];
-    }
-
-    let sentences = [
-        
-        'O tempo não espera pela gente Mas eu espero por ti O tempo quer ser indiferente Só eu te quero aqui.    Por mais que eu te diga Mesmo que eu consiga O tempo não espera por mim   O tempo não espera pela gente Se não fugirmos daqui Sei que não vai ser diferente só por dizer que sim Querer-te despida Na pele de rendida No meu tempo não era assim Mas por mais que a vida',
-        'Minha saudade deita-se tarde, Se tu não vens E acorda cedo, com esse medo de não voltares contigo assim, contigo assim,perto de mim o mundo existe mas se não vens, mas se não vens eu vou ao fundo e fico triste O tempo tarda e tudo é nada longe de ti e ao ver-te o beijo morde o desejo de estares aqui Quem sabe um dia a fantasia da minha dor seja a alegria da eterna noite e muito amor',
-        'Eu já sei quem sou E o que fiz Foi para o bem de mim Sou o que nao fui E serei o amanhã a dois Onde é que vais Espera por mim Eu vou ser melhor por ti Sempre que sais Eu sinto o fim  Eu nao quero ficar por aqui Tu, dona de ti Deixas me assim Sem saber o que fazer És, o que mais quero Se fores eu espero A vida sem ti nao tem sabor',
-        'Eu quero estar Mais próximo do teu olhar E viajar nesse mundo que só nos teus olhos eu posso ver Eu quero gritar Ainda que me falte a voz Ou te dizer bem baixinho no ouvido que agora é a nossa vez Vou Marcar no meu corpo a frase mais bela que existe de amor E prometer nos meus beijos Que só nos teus beijos Eu quero viver E quando acordares eu quero lá estar',
-        'Eu não quero pagar por aquilo que eu não fiz Não me fazem ver que a luta é pelo meu país Eu não quero pagar depois de tudo o que dei Não me fazem ver que fui eu que errei Não fui eu que gastei Mais do que era para mim Não fui eu que tirei Não fui eu que comi Não fui eu que comprei Não fui eu que escondi Quando estavam a olhar Não',
-        'Como é bom estar contigo no sofá Neve lá fora e uma chávena de chá Eu vejo um filme, tu adormeces Conto-te as séries de que te esqueces E o meu resumo que é tão bom Hora e meia a ouvir o mesmo som E é por isso que eu sou apenas teu'
-        
-    ];
-
-    btn.onclick = function () {
-        console.log(getRandomSentence());
-        text.textContent = getRandomSentence();
-    }
-
 });
 
 
